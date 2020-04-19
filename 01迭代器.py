6192b2af64cb82ae92e64f3605cdf4a340494f09
lin <584148675@qq.com>
2020-04-19 16:43:42 +0800

    測試

--- 01迭代器.py
+++ 01迭代器.py
@@ -1,11 +1,4 @@
 import sys
-list=['a','b','c','d','e']
-it=iter(list)
-while True:
-    try:
-        print(next(it))
-    except StopIteration:
-        sys.exit()
 
 class MyNuber:
     def __iter__(self):#返回一個迭代器對象
@@ -16,4 +9,23 @@ class MyNuber:
         x=self.a
         self.a+=1
         return self
+
+myclass=MyNuber()
+myiter=iter(myclass)
+
+for j in range(5):
+    print(next(myiter),end=",")
+
+list=['a','b','c','d','e']
+it=iter(list)
+while True:
+    try:
+        print(next(it))
+    except StopIteration:
+        sys.exit()
+
+
+
+
+
 
