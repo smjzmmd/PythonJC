1d08016b548835abf3898036ffb2ad6eab8526ca
lin <584148675@qq.com>
2020-04-19 16:57:52 +0800

    迭代器

--- 01迭代器.py
+++ 01迭代器.py
@@ -1,20 +1,23 @@
 import sys
 
 class MyNuber:
-    def __iter__(self):#返回一個迭代器對象
-        self.a=1
+    def __iter__(self):  # 返回一個迭代器對象
+        self.a = 1#
         return self
 
-    def __next__(self):#返回下一個迭代器對象
-        x=self.a
-        self.a+=1
-        return self
+    def __next__(self):  # 返回下一個迭代器對象
+        x = self.a
+        self.a += 1
+        return x
+
 
 myclass=MyNuber()
 myiter=iter(myclass)
 
 for j in range(5):
     print(next(myiter),end=",")
+    if j==4:
+        print()
 
 list=['a','b','c','d','e']
 it=iter(list)
