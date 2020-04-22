04eb591553f6506678e11cad216f5a6c78dc5274
lin <584148675@qq.com>
2020-04-22 15:36:06 +0800

    迭代器

---
+++ 列表元組字典集合.py
@@ -0,0 +1,24 @@
+list1=["google","裂開","hello","python","@@","/*****/","-----++++++"]#列表
+
+#訪問列表中的元素
+print("list1[1]:",list1[1])
+print("list1[1:3]:",list1[1:3])
+print("list1[1][1:2]:",list1[1][1:2])
+print()
+
+#修改list列表指定位置的數據
+print("list[4]的值：",list1[4])
+list1[4]="我改"
+print("修改完後list[4]：",list1[4])
+print()
+
+#刪除list1中的某個元素
+list2=["a","b","c","d"]
+print("原始數據：",list2)
+del list2[len(list2)-1]
+print("刪除後：",list2)
+print()
+
+#列表操作
+
+
